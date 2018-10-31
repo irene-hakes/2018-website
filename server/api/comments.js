8bfc4888a3c6a51dbf4e29856763e5cc534c3367
Irene Hakes <irenebrbr@gmail.com>
2018-10-31 13:42:31 -0400

    added api routes

---
+++ server/api/comments.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const Comments = require('../db/models/comments')
+module.exports = router
+
+router.post('/:id', async (req, res, next) => {
+  try {
+    const newComment = await Comments.create(req.body)
+
+    res.status(201).json(newComment)
+    //make it so it redirects to a page that says your message was sent!
+  } catch (err) {
+    next(err)
+  }
+})
